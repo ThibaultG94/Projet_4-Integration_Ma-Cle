a7144619a9d7accaca02bacf29625be408528232
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-26 12:44:12 +0200

    responsive navbar...

--- scripts/header.js
+++ scripts/header.js
@@ -2,8 +2,8 @@ let navHeader = document.querySelector(".header");
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 50 && window.scrollY > lastScroll) {
-    navHeader.style.transform = "translateY(-150px)";
+  if (window.scrollY > 10 && window.scrollY > lastScroll) {
+    navHeader.style.transform = "translateY(-120px)";
   }
 
   if (window.scrollY < 120 && window.scrollY < lastScroll) {
