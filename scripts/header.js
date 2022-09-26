a18f21582f28be61500e76f57f0a3fdd4048d88b
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-26 11:50:23 +0200

    scroll bar event

--- scripts/header.js
+++ scripts/header.js
@@ -2,10 +2,13 @@ let navHeader = document.querySelector(".header");
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY < lastScroll) {
+  if (window.scrollY > 50 && window.scrollY > lastScroll) {
     navHeader.style.transform = "translateY(-200px)";
-  } else if (window.scrollY > lastScroll) {
+  }
+
+  if (window.scrollY < 120 && window.scrollY < lastScroll) {
     navHeader.style.transform = "translateY(0)";
   }
+
   lastScroll = window.scrollY;
 });
