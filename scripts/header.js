facf9dfca17ac9013aac6f6f2432e02846d3c795
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-27 11:52:57 +0200

    box-shadow header-anim

--- scripts/header.js
+++ scripts/header.js
@@ -1,12 +1,15 @@
 let navHeader = document.querySelector(".header");
 let lastScroll = 0;
+const shadowHeader = document.querySelector(".shadow-header");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 10 && window.scrollY > lastScroll) {
-    navHeader.style.transform = "translateY(-120px)";
+    navHeader.style.transform = "translateY(-100%)";
+    navHeader.classList.add("shadow-header");
   }
 
   if (window.scrollY < 120 && window.scrollY < lastScroll) {
+    navHeader.classList.remove("shadow-header");
     navHeader.style.transform = "translateY(0)";
   }
 
