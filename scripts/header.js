3a1ae4c267bb18f590ee90ac59503019b6ad837f
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-10-04 12:41:37 +0200

    animation items javascript

--- scripts/header.js
+++ scripts/header.js
@@ -4,13 +4,16 @@ const shadowHeader = document.querySelector(".shadow-header");
 
 const toggleMenu = document.querySelector(".header-nav_toggle-menu");
 const navMenu = document.querySelector(".header-nav_toggle-menu_ul");
-const navItems = document.querySelectorAll(
-  ".header-nav_container-list_item > a"
-);
 const darkWindow = document.querySelector(".dark-window");
 const homeItem = document.querySelector(
   ".header-nav_container-list_item--home-link"
 );
+const firstItem = document.querySelector(".first-item");
+const secondtItem = document.querySelector(".second-item");
+const thirdItem = document.querySelector(".third-item");
+const fourthItem = document.querySelector(".fourth-item");
+const fifthItem = document.querySelector(".fifth-item");
+const sixthItem = document.querySelector(".sixth-item");
 
 //--------------
 // NAVBAR SCROLL
@@ -39,7 +42,10 @@ toggleMenu.addEventListener("click", () => {
   darkWindow.classList.toggle("clicked-height");
   navMenu.classList.toggle("clicked-opacity");
   homeItem.classList.toggle("clicked-translateX");
-  navItems.forEach((item) => {
-    item.classList.toggle("clicked-translateX");
-  });
+  firstItem.classList.toggle("clicked-translateX");
+  secondtItem.classList.toggle("clicked-translateX");
+  thirdItem.classList.toggle("clicked-translateX");
+  fourthItem.classList.toggle("clicked-translateX");
+  fifthItem.classList.toggle("clicked-translateX");
+  sixthItem.classList.toggle("clicked-translateX");
 });
